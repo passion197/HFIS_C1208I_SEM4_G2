2ce4e6ffb4f1e6afb3fefafa2b6ac0232de613e3
Doll <Doll@10.22.32.196>
2014-07-18 16:25:09 +0700

    ThanhTT- Booking Step 2

--- src/java/da/ConnectionDB.java
+++ src/java/da/ConnectionDB.java
@@ -23,7 +23,7 @@ public class ConnectionDB {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String url = "jdbc:sqlserver://localhost:56190;databaseName=East2West";
-            System.out.println("sss");
+            System.out.println("ssshhh");
             con = DriverManager.getConnection(url, "sa", "123456");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
